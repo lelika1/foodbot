1a624a2e2bfe1f4a4b4e80c3b534c77f5eefe36e
Olga Sycheva <lelika1@gmail.com>
2020-10-17 21:08:55 +0200

    Added base entities. Report last 7d stats.

--- cmd/foodbot/main.go
+++ cmd/foodbot/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"time"
+
+	"github.com/lelika1/foodbot/internal/foodbot"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -13,6 +13,8 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("usage: %s bot_token", os.Args[0])
 	}
+
+	db := foodbot.NewDB()
 
 	bot, err := tgbotapi.NewBotAPI(os.Args[1])
 	if err != nil {
@@ -40,7 +42,8 @@ func main() {
 		case "/stat":
 			msg.Text = "You ate today:"
 		case "/stat7":
-			msg.Text = fmt.Sprintf("You ate since %v", time.Now().AddDate(0, 0, -6).Format("2006/01/02"))
+			msg.Text = weeklyReport(update.Message.From.UserName, db)
+			msg.ParseMode = "MarkdownV2"
 		default:
 			msg.ReplyToMessageID = update.Message.MessageID
 		}
@@ -48,4 +51,17 @@ func main() {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		bot.Send(msg)
 	}
+}
+
+func weeklyReport(username string, db *foodbot.DB) string {
+	report, err := db.WeeklyReport(username)
+	switch err {
+	case nil:
+		return report
+	case foodbot.ErrUserNotFound:
+		return "You aren't a user of this bot."
+	}
+
+	log.Printf("WeeklyReport(%v) failed with %v", username, err)
+	return "Something went wrong. Try later."
 }
---
+++ internal/foodbot/foodbot.go
@@ -0,0 +1,99 @@
+package foodbot
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+// Report ...
+type Report struct {
+	Product string
+	Kcal    uint32 // for 100g
+	Grams   uint32
+}
+
+// Day ...
+type Day struct {
+	Reports []Report
+}
+
+// TotalKcal ...
+func (d *Day) TotalKcal() uint32 {
+	var ret uint32
+	for _, r := range d.Reports {
+		ret += r.Kcal * r.Grams / 100
+	}
+	return ret
+}
+
+// User ...
+type User struct {
+	Name    string
+	Limit   uint32
+	History map[string]uint32 // "2006/01/02" -> kcal consumed
+	Today   Day
+}
+
+// NewUser ...
+func NewUser(name string, limit uint32) *User {
+	return &User{
+		Name:    name,
+		Limit:   limit,
+		History: make(map[string]uint32),
+	}
+}
+
+// DB ...
+type DB struct {
+	Users map[string]*User
+}
+
+// NewDB ...
+func NewDB() *DB {
+	return &DB{Users: map[string]*User{
+		"osycheva": NewUser("osycheva", 1546),
+	}}
+}
+
+// ErrUserNotFound ...
+var ErrUserNotFound = errors.New("user was not found")
+
+// User ...
+func (db *DB) User(name string) (*User, error) {
+	if user, ok := db.Users[name]; ok {
+		return user, nil
+	}
+
+	return nil, ErrUserNotFound
+}
+
+// WeeklyReport ...
+func (db *DB) WeeklyReport(username string) (string, error) {
+	user, err := db.User(username)
+	if err != nil {
+		return "", err
+	}
+
+	var sb strings.Builder
+
+	total := user.Today.TotalKcal()
+	fmt.Fprintf(&sb, "`%s Today:         ` *%v kcal*\n", color(total, user.Limit), total)
+
+	now := time.Now()
+	for delta := 1; delta <= 6; delta++ {
+		key := now.AddDate(0, 0, -delta).Format("Mon 2006/01/02")
+		kcal := user.History[key]
+		fmt.Fprintf(&sb, "`%s %v:` *%v kcal*\n", color(kcal, user.Limit), key, kcal)
+	}
+	return sb.String(), nil
+
+}
+
+func color(val, limit uint32) string {
+	if val < limit {
+		return "✅"
+	}
+	return "❌"
+}
