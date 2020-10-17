381fc392b6a5a4958a30669996d57b4bb07969fb
Olga Sycheva <lelika1@gmail.com>
2020-10-17 17:59:18 +0200

    Added a simple example of a telegram bot.

--- cmd/foodbot/main.go
+++ cmd/foodbot/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-	fmt.Println("Hello food")
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %s bot_token", os.Args[0])
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Args[1])
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
